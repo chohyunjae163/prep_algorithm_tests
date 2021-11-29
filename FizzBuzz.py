1516a0f738f44503a15c541f5abebe0ce1836438
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2021-11-29 16:23:30 +0900

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+def FizzBuzz(max):
+    for n in (range(1,max)):
+        if n % 15 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+        else:    
+            print(n)
