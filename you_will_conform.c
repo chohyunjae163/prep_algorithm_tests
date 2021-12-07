794b4dee4f25b0f79b6e36432f1acf273a55010d
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2021-12-08 00:51:54 +0900

    Update you_will_conform.c

--- you_will_conform.c
+++ you_will_conform.c
@@ -1,41 +1,48 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+// Programming for the Puzzled by Srini Devadas
+// Chapter 1 : You Will All Conform
+// generate the minimum number of commands.
+// conflate contiguous people with the same cap orientations by grouping them into an interval. After the intervals have been determined,
+// we have exactly the same situation as the alternating people above -- there will be m forward intervals interspersed between m, m-1, or
+// m + 1 backward intervals. choose the orientation that results in fewer commands.
 
 #include <stdio.h>
-//Programming for the Puzzled
-// by Srini Devadas
-// You Will All Conform
 
-char caps_1[13] = { 'F','F','B','B','B','F','B','B','B','F','F','B','F'};
-char caps_2[13] = { 'F','F','B','B','B','F','B','B','B','F','F','F','F'};
-void pleaseConform(char*);
+// input samples
+char caps_1[13] = {'F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'B', 'F'};  // 3
+char caps_2[13] = {'F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F'};  // 2
 
-typedef struct Interval {
-    int start;
-    int end;
-    char label;
-} Interval;
-
-typedef struct Node* PNode;
-typedef struct Node {
-    Interval interval;
-    PNode next;
-} Node;
-
+int pleaseConform(char *);
 int main()
 {
-    printf("Hello World");
-   
+    int numCommands = pleaseConform(caps_2);
+    printf("numCommands : %d \n", numCommands);
     return 0;
 }
 
-void pleaseConform(char* caps)
+int pleaseConform(char *caps)
 {
-    int start, forward, backward = 0;
-     
+    // get the size of array
+    size_t size = sizeof(caps_1) / sizeof(char);
+    printf("size : %d \n", size);
+    char latest = ' ';
+    int forward = 0;
+    int backward = 0; // orientation group counts.
+    for (int current = 0; current < size; ++current)
+    {
+        if (latest != *(caps + current))
+        {
+            if (*(caps + current) == 'F')
+            {
+                ++forward;
+            }
+            else if (*(caps + current) == 'B')
+            {
+                ++backward;
+            }
+        }
+        latest = *(caps + current);
+    }
+    printf("forward : %d \n", forward);
+    printf("backward : %d \n", backward);
+    return forward <= backward ? forward : backward;
 }
